1515b133e287846ce9e61cf0fe558ba72a20ce2d
Daniel Green <dan2552@gmail.com>
2016-12-07 13:41:50 +0000

    Adds test main/1: prints a multiplication table of the supplied size

--- test/prime_numbers_exercise_test.exs
+++ test/prime_numbers_exercise_test.exs
@@ -1,4 +1,17 @@
 defmodule PrimeNumbersExerciseTest do
   use ExUnit.Case
   doctest PrimeNumbersExercise
+  import ExUnit.CaptureIO
+
+  test "main/1: prints a multiplication table of the supplied size" do
+    capure = capture_io(fn -> PrimeNumbersExercise.main(["2"]) end)
+
+    expectation = String.trim_trailing("""
+    |    |  1 |  2 |
+    |  1 |  1 |  2 |
+    |  2 |  2 |  4 |
+    """)
+
+    assert capure == expectation
+  end
 end
