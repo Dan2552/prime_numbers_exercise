361396e202b0885a2165c9c80ac33dbf05b24b65
Daniel Green <dan2552@gmail.com>
2016-12-07 09:49:20 +0000

    Makes TablePrinter print alignment test green

--- lib/prime_numbers_exercise/table_printer.ex
+++ lib/prime_numbers_exercise/table_printer.ex
@@ -1,10 +1,36 @@
 defmodule PrimeNumbersExercise.TablePrinter do
+  import Enum, only: [map: 2, map_join: 3, max_by: 2, with_index: 1, at: 2]
+
   def print(rows) do
-    lines = for row <- rows, do: print_line(row)
-    Enum.join(lines, "\n")
+    widths = calculate_column_widths(rows)
+    map_join(rows, "\n", &print_line(&1, widths))
   end
 
-  defp print_line(row) do
-    "|  " <> Enum.join(row, " |  ") <> " |"
+  defp print_line(row, widths) do
+    row
+    |> with_index
+    |> map_join(" |  ", &cell_contents(&1, widths))
+    |> format_with_side_borders
+  end
+
+  defp calculate_column_widths(rows) do
+    List.zip(rows) |> map(&column_max_length(&1))
+  end
+
+  defp column_max_length(col) do
+    col
+    |> Tuple.to_list
+    |> max_by(&String.length(&1))
+    |> String.length
+  end
+
+  defp format_with_side_borders(string) do
+    "|  " <> string <> " |"
+  end
+
+  defp cell_contents(tuple, widths) do
+    {contents, index} = tuple
+    width = at(widths, index)
+    String.rjust(contents, width)
   end
 end
