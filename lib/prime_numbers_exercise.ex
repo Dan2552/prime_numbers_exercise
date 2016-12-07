7f8270bbeef82299dc32b437829f27d7a925f537
Daniel Green <dan2552@gmail.com>
2016-12-07 17:27:44 +0000

    Change original main/1 to only use prime numbers

--- lib/prime_numbers_exercise.ex
+++ lib/prime_numbers_exercise.ex
@@ -3,14 +3,19 @@ defmodule PrimeNumbersExercise do
     args
     |> hd
     |> String.to_integer
-    |> generate_table
-    |> PrimeNumbersExercise.TablePrinter.print
-    |> IO.puts
+    |> generate_prime_table
+    |> print_table
   end
 
-  defp generate_table(amount_of_numbers) do
-    1..amount_of_numbers
-    |> Enum.to_list
+  defp generate_prime_table(amount_of_numbers) do
+    amount_of_numbers
+    |> PrimeNumbersExercise.PrimeNumbers.generate
     |> PrimeNumbersExercise.MultiplicationTable.generate_table
+  end
+
+  defp print_table(table) do
+    table
+    |> PrimeNumbersExercise.TablePrinter.print
+    |> IO.puts
   end
 end
--- test/prime_numbers_exercise_test.exs
+++ test/prime_numbers_exercise_test.exs
@@ -3,13 +3,13 @@ defmodule PrimeNumbersExerciseTest do
   doctest PrimeNumbersExercise
   import ExUnit.CaptureIO
 
-  test "main/1: prints a multiplication table of the supplied size" do
+  test "main/1: prints a multiplication table of the supplied size, skipping numbers that aren't prime" do
     capure = capture_io(fn -> PrimeNumbersExercise.main(["2"]) end)
 
     expectation = """
-    |    |  1 |  2 |
-    |  1 |  1 |  2 |
-    |  2 |  2 |  4 |
+    |    |  2 |  3 |
+    |  2 |  4 |  6 |
+    |  3 |  6 |  9 |
     """
 
     assert capure == expectation
